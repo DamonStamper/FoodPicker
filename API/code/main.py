3c2c88ad3a06f360c65730a160e12eacd7ab65ee
Damon Stamper <Damon.Stamper@us.abb.com>
2019-08-23 13:45:49 -0400

    Fixing API container to return something at default path. Setting up /healthz endpoint

--- API/code/main.py
+++ API/code/main.py
@@ -1,89 +1,17 @@
 from flask import Flask, request, jsonify
-from flask_sqlalchemy import SQLAlchemy
-from flask_marshmallow import Marshmallow
+from flask_api import status
 import os
 
 app = Flask(__name__)
-basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'crud.sqlite')
-db = SQLAlchemy(app)
-ma = Marshmallow(app)
 
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True)
-    email = db.Column(db.String(120), unique=True)
-
-    def __init__(self, username, email):
-        self.username = username
-        self.email = email
-
-
-class UserSchema(ma.Schema):
-    class Meta:
-        # Fields to expose
-        fields = ('username', 'email')
-
-
-user_schema = UserSchema()
-users_schema = UserSchema(many=True)
-
+@app.route("/")
+def hello():
+    return "Hello World!"
 
 # endpoint to check health of applicaion
-@app.route("/user/<id>", methods=["GET"])
-
-# endpoint to create new user
-@app.route("/user", methods=["POST"])
-def add_user():
-    username = request.json['username']
-    email = request.json['email']
-    
-    new_user = User(username, email)
-
-    db.session.add(new_user)
-    db.session.commit()
-
-    return jsonify(new_user)
-
-
-# endpoint to show all users
-@app.route("/user", methods=["GET"])
-def get_user():
-    all_users = User.query.all()
-    result = users_schema.dump(all_users)
-    return jsonify(result.data)
-
-
-# endpoint to get user detail by id
-@app.route("/user/<id>", methods=["GET"])
-def user_detail(id):
-    user = User.query.get(id)
-    return user_schema.jsonify(user)
-
-
-# endpoint to update user
-@app.route("/user/<id>", methods=["PUT"])
-def user_update(id):
-    user = User.query.get(id)
-    username = request.json['username']
-    email = request.json['email']
-
-    user.email = email
-    user.username = username
-
-    db.session.commit()
-    return user_schema.jsonify(user)
-
-
-# endpoint to delete user
-@app.route("/user/<id>", methods=["DELETE"])
-def user_delete(id):
-    user = User.query.get(id)
-    db.session.delete(user)
-    db.session.commit()
-
-    return user_schema.jsonify(user)
+@app.route("/healthz")
+def healthz():
+    return "OK", status.HTTP_200_OK
 
 if __name__ == "__main__":
     if "DEBUG" in os.environ:
